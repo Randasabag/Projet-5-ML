73eb15282bf4fe56fa09aa270058ae0778f0e04a
Randa ALSABBAGH <alsabbaghranda@gmail.com>
2024-08-30 12:49:01 +0200

    updates app & api

--- api.py
+++ api.py
@@ -1,36 +1,57 @@
-import streamlit as st
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import joblib
 import tensorflow as tf
 import tensorflow_hub as hub
+import mlflow.pyfunc
+import mlflow
 
-# Charger le modèle enregistré
-model = joblib.load("use_model.pkl")
+
+# Charger le modèle depuis MLflow
+def load_model():
+    # URI du modèle MLflow
+    model_uri = "runs:/09259b68390948849fd6beffe80015b0/model"
+    model = mlflow.pyfunc.load_model(model_uri)
+    return model
+
+model = load_model()
+
+# Charger votre modèle de machine learning et le MultiLabelBinarizer
+#model = joblib.load("/Users/randaalsabbagh/Desktop/MACHINE_LEARNING/P5/Projet-5-ML/use_model.pkl")  # Chemin vers ton modèle sauvegardé
 mlb = joblib.load("mlb.pkl")  # Charger le MultiLabelBinarizer
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
 
-# Titre de l'application
-st.title("Générateur de Tags")
+# Initialiser l'application FastAPI
+app = FastAPI()
+print("FastAPI utilisé")
 
-# Champ de saisie pour la question
-question = st.text_input("Posez votre question:")
+class Question(BaseModel):
+    question: str
 
-# Si une question est posée, générer des tags
-if st.button("Générer des tags"):
-    if question:
-        # Prétraitement si nécessaire
-        input_data = embed([question]) # Ajuster selon le modèle
+class TagsResponse(BaseModel):
+    predicted_tags: list[str]
+
+@app.post("/generate_tags", response_model=TagsResponse)
+def generate_tags(question: Question):
+    try:
+        # Prétraitement de la question
+        input_data = embed([question.question])  # Modifie ceci selon le format attendu par ton modèle
         
-        # Prédire les tags avec le modèle
+        # Faire une prédiction avec le modèle
         predicted_tags = model.predict(input_data)
+        print("donne moi les tags predits:", predicted_tags)
         
-        # Convertir les prédictions en tags lisibles
-        if predicted_tags.ndim == 2:  # Si les résultats sont binaires (multilabel)
+        # Convertir les prédictions en labels lisibles
+        if predicted_tags.ndim == 2:  # Si le modèle renvoie une matrice binaire
             predicted_tags = mlb.inverse_transform(predicted_tags)
         else:
             predicted_tags = mlb.inverse_transform(predicted_tags.reshape(1, -1))
         
-        # Afficher les tags générés
-        st.write("Tags générés :", predicted_tags[0])
-    else:
-        st.write("Veuillez entrer une question.")
+        # Convertir les prédictions en liste de chaînes
+        tags_list = [f"#{tag}" for sublist in predicted_tags for tag in sublist]
+        
+        # Renvoyer les tags comme réponse
+        return TagsResponse(tags=tags_list)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- app.py
+++ app.py
@@ -1,40 +1,28 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-import joblib
-import tensorflow as tf
-import tensorflow_hub as hub
+import streamlit as st
+import requests
 
 
-# Charger votre modèle de machine learning et le MultiLabelBinarizer
-model = joblib.load("use_model.pkl")  # Chemin vers ton modèle sauvegardé
-mlb = joblib.load("mlb.pkl")  # Charger le MultiLabelBinarizer
-embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+# Titre de l'application
+st.title("Générateur de Tags")
 
 
-# Initialiser l'application FastAPI
-app = FastAPI()
-print("FastAPI utilisé")
+# Champ de saisie pour la question
+question = st.text_input("Posez votre question:")
 
-class Question(BaseModel):
-    question: str
 
-@app.post("/generate_tags")
-def generate_tags(question: Question):
-    try:
-        # Prétraitement de la question si nécessaire
-        input_data = embed([question.question])  # Modifie ceci selon le format attendu par ton modèle
-        
-        # Faire une prédiction avec le modèle
-        predicted_tags = model.predict(input_data)
-        
-        # Convertir les prédictions en labels lisibles
-        # Si 'predicted_tags' est une matrice binaire, utilisez mlb.inverse_transform
-        if predicted_tags.ndim == 2:  # Si le modèle renvoie une matrice binaire
-            predicted_tags = mlb.inverse_transform(predicted_tags)
-        else:
-            predicted_tags = mlb.inverse_transform(predicted_tags.reshape(1, -1))
-        
-        # Renvoyer les tags comme réponse
-        return {"tags": predicted_tags[0].tolist()}  # Convertir en liste si nécessaire
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+# Si une question est posée, générer des tags
+if st.button("Générer des tags"):
+
+   
+    response = requests.post('http://127.0.0.1:8000/generate_tags', json={"question": question})
+    data = response.json()
+
+    # Afficher les hashtags
+    if 'tags' in data:
+                st.subheader("Tags générés :")
+                tags = data['tags']
+                for tag in tags:
+                    hashtag = f"#{tag}"
+                    st.markdown(f'<span style="color: #007BFF;">{hashtag}</span>', unsafe_allow_html=True)
+    else:
+                st.error("Les tags ne sont pas dans le format attendu.")
